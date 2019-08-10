5d8d5c7c8ef397dfd88137f032af477107b61d62
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-08-10 09:43:38 -0700

    Implement searching algorithm on binary file

--- src/analyzer/Main.java
+++ src/analyzer/Main.java
@@ -1,30 +1,61 @@
 package analyzer;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-        // java Main doc.pdf "%PDF-" "PDF document"
+        // e.g.:  java Main doc.pdf "%PDF-" "PDF document"
         String filepath = args[0];
         String searchPattern = args[1];
         String fileType = args[2];
 
+        byte[] searchBytes = searchPattern.getBytes();
+
+        boolean found = false;
+
         try (InputStream inputStream = new FileInputStream(filepath)) {
-            Pattern p = Pattern.compile(searchPattern);
+            // get file size
+            long fileSize = new File(filepath).length();
+            // create byte array to store file
+            byte[] allBytes = new byte[(int) fileSize];
+            // read full file into array
+            inputStream.read(allBytes);
+            // get length of search argument
+            int searchLength = searchBytes.length;
 
+            int i = 0;
+            int j = 0;
+            // search for for subarray searchBytes in allBytes array. O(n)
+            while(i < fileSize && j < searchLength) {
+                if (allBytes[i] == searchBytes[j]) {
+                    i++;
+                    j++;
 
-            Matcher m = p.matcher();
+                    // if search pattern array completely traversed
+                    if (j == searchLength) {
+                        found = true; // subarray exists in allBytes array
+                        break;
+                    }
+                }
+                // if bytes do not match, start search with one byte forward offset in file
+                else {
+                    i++;
+                    j = 0;
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-        System.out.println("Hello World!");
+        if (found) {
+            System.out.println(fileType);
+        } else {
+            System.out.println("Unknown file type");
+        }
     }
 }
