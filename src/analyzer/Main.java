ac73e7e23b675612212644f0915297a5c5213d4c
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-08-14 17:50:27 -0700

    Update main to work with strategy pattern for algorithms

--- src/analyzer/Main.java
+++ src/analyzer/Main.java
@@ -8,45 +8,34 @@ import java.io.InputStream;
 public class Main {
 
     public static void main(String[] args) {
-        // e.g.:  java Main doc.pdf "%PDF-" "PDF document"
-        String filepath = args[0];
-        String searchPattern = args[1];
-        String fileType = args[2];
+        // java Main --KMP huge_doc.pdf "%PDF-" "PDF document"
+        String selectedAlgorithm = args[0];
+        String filepath = args[1];
+        String searchPattern = args[2];
+        String fileType = args[3];
 
-        byte[] searchBytes = searchPattern.getBytes();
-
+        Finder finder;
+        byte[] patternByteArray = searchPattern.getBytes();
         boolean found = false;
 
+        switch (selectedAlgorithm) {
+            case "--naive":
+                finder = new Finder(new NaiveStrategy());
+                break;
+            case "--KMP":
+                finder = new Finder(new KnuthMorrisPrattStrategy());
+                break;
+            default:
+                System.out.println("Invalid algorithm selected.");
+                return;
+        }
+
         try (InputStream inputStream = new FileInputStream(filepath)) {
-            // get file size
-            long fileSize = new File(filepath).length();
-            // create byte array to store file
-            byte[] allBytes = new byte[(int) fileSize];
-            // read full file into array
-            inputStream.read(allBytes);
-            // get length of search argument
-            int searchLength = searchBytes.length;
+            long fileSize = new File(filepath).length(); // get size of file
+            byte[] fileByteArray = new byte[(int) fileSize]; // initialize array with size of file
+            inputStream.read(fileByteArray); // read file into array
 
-            int i = 0;
-            int j = 0;
-            // search for for subarray searchBytes in allBytes array. O(n)
-            while(i < fileSize && j < searchLength) {
-                if (allBytes[i] == searchBytes[j]) {
-                    i++;
-                    j++;
-
-                    // if search pattern array completely traversed
-                    if (j == searchLength) {
-                        found = true; // subarray exists in allBytes array
-                        break;
-                    }
-                }
-                // if bytes do not match, start search with one byte forward offset in file
-                else {
-                    i++;
-                    j = 0;
-                }
-            }
+            found = finder.containsPattern(fileByteArray, patternByteArray);
 
         } catch (IOException e) {
             e.printStackTrace();
