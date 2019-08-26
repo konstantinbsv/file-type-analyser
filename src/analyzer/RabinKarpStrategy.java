63279c76b81f18f32a35d42a2a6ac0ba162b1af0
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-08-26 07:40:05 -0700

    Create RabinKarpStrategy concrete strategy class

---
+++ src/analyzer/RabinKarpStrategy.java
@@ -0,0 +1,9 @@
+package analyzer;
+
+public class RabinKarpStrategy implements IAlgorithmStrategy{
+
+    @Override
+    public boolean search(byte[] fileArray, byte[] patternArray) {
+        return false;
+    }
+}
